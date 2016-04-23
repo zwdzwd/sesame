a7362fa6ac8b101305386b0e739185945e2887be
zwdzwd <zhouwanding@gmail.com>
2016-04-23 19:46:19 -0400

    update to data and some bug fixes

--- data-raw/manifest.R
+++ data-raw/manifest.R
@@ -11,7 +11,7 @@ save(hm450.hg19.probe2chr, file='../data/hm450.hg19.probe2chr.rda')
 
 save(hm450.ordering, file='../data/hm450.ordering.rda')
 save(hm450.controls, file='../data/hm450.controls.rda')
-
+save(hm450.mask, file='../data/hm450.mask.rda')
 
 ## for EPIC
 
