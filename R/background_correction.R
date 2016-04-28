59aaa816c85b095ca760ee3072874c160335b3dd
zwdzwd <zhouwanding@gmail.com>
2016-04-27 22:20:40 -0400

    update to hm27 maskers

--- R/background_correction.R
+++ R/background_correction.R
@@ -82,8 +82,14 @@ BackgroundCorrectionNoob <- function(sset, offset=15) {
 }
 
 .GetNormCtls <- function(sset) {
-  normctl.G <- sset$ctl[grep('norm_(c|g)',tolower(rownames(sset$ctl))),]
-  normctl.R <- sset$ctl[grep('norm_(a|t)',tolower(rownames(sset$ctl))),]
+  if (sset$platform == 'hm27') {
+    ## controversial to use, maybe the mean of all signals in each channel?
+    normctl.G <- sset$ctl[grep('norm.green', tolower(rownames(sset$ctl))),]
+    normctl.R <- sset$ctl[grep('norm.red', tolower(rownames(sset$ctl))),]
+  } else {                              # hm450 and EPIC
+    normctl.G <- sset$ctl[grep('norm_(c|g)',tolower(rownames(sset$ctl))),]
+    normctl.R <- sset$ctl[grep('norm_(a|t)',tolower(rownames(sset$ctl))),]
+  }
   c(G=mean(normctl.G$G, na.rm=TRUE), R=mean(normctl.R$R, na.rm=TRUE))
 }
 
@@ -98,39 +104,29 @@ DyeBiasCorrectionMostBalanced <- function(ssets) {
   normctls <- vapply(ssets, .GetNormCtls, numeric(2))
   most.balanced <- which.min(abs(normctls['G',] / normctls['R',] - 1))
   ref <- mean(normctls[,most.balanced], na.rm=TRUE)
-  DyeBiasCorrection(ssets, ref, normctls=normctls)
+  lapply(ssets, function(sset) DyeBiasCorrection(sset, ref))
 }
 
 #' Correct dye bias
 #'
 #' Correct dye bias
 #'
-#' @param ssets a list of \code{SignalSet}s
+#' @param sset a \code{SignalSet}s
 #' @param ref reference signal level
-#' @return a list of normalized \code{SignalSet}s
+#' @return a normalized \code{SignalSet}s
 #' @export
-DyeBiasCorrection <- function(ssets, ref, normctls=NULL) {
-  if (is.null(normctls))       # recompute normal control if not given
-    normctls <- vapply(ssets, .GetNormCtls, numeric(2))
-  factor <- ref / normctls
-  ssets.n <- lapply(names(ssets), function(sample.name) {
-    n <- list()
-    sset <- ssets[[sample.name]]
-    fR <- factor['R', sample.name]
-    fG <- factor['G', sample.name]
-    n$IR <- matrix(c(fR*sset$IR[,'M'], fR*sset$IR[,'U']),
-                   nrow=nrow(sset$IR), dimnames=dimnames(sset$IR))
-    n$IG <- matrix(c(fG*sset$IG[,'M'], fG*sset$IG[,'U']),
-                   nrow=nrow(sset$IG), dimnames=dimnames(sset$IG))
-    n$II <- matrix(c(fG*sset$II[,'M'], fR*sset$II[,'U']),
-                   nrow=nrow(sset$II), dimnames=dimnames(sset$II))
-    n$ctl <- transform(sset$ctl, G=fG*G, R=fR*R)
-
-    ## not sure if we need to normalize oobs, it is dye-bias corrected anyway
-    n$oobG <- fG*sset$oobG
-    n$oobR <- fR*sset$oobR
-    n
-  })
-  names(ssets.n) <- names(ssets)
-  ssets.n
+DyeBiasCorrection <- function(sset, ref) {
+  normctl <- .GetNormCtls(sset)
+  fR <- ref/normctl['R']
+  fG <- ref/normctl['G']
+  SignalSet(sset$platform,
+            IG = matrix(c(fG*sset$IG[,'M'], fG*sset$IG[,'U']),
+              nrow=nrow(sset$IG), dimnames=dimnames(sset$IG)),
+            IR = matrix(c(fR*sset$IR[,'M'], fR*sset$IR[,'U']),
+              nrow=nrow(sset$IR), dimnames=dimnames(sset$IR)),
+            II = matrix(c(fG*sset$II[,'M'], fR*sset$II[,'U']),
+              nrow=nrow(sset$II), dimnames=dimnames(sset$II)),
+            ctl = transform(sset$ctl, G=fG*G, R=fR*R),
+            oobG = fG*sset$oobG,
+            oobR = fR*sset$oobR)
 }
