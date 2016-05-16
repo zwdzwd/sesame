df897420b5ac36428e987967ef5a0ec2ad51a64d
zwdzwd <zhouwanding@gmail.com>
2016-05-16 13:38:43 -0400

    minor, message omitted

--- R/funnorm.R
+++ R/funnorm.R
@@ -27,94 +27,6 @@ QuantileSet <- function(
     X.all.M=X.all.M, X.all.U=X.all.U), class="QuantileSet")
 }
 
-#' build control matrix for funnorm
-#' 
-#' build control matrix for funnorm
-#'
-#' @param sset an object of class SignalSet
-#' @return a vector with control summaries
-#' @export
-BuildControlMatrix450k <- function(sset) {
-  
-  ctls <- split(sset$ctl, sset$ctl$type)
-
-  cm <- NULL
-
-  ## bisulfite conversion type II
-  cm <- c(cm, bisulfite2=mean(ctls[['BISULFITE CONVERSION II']]$R, na.rm=TRUE))
-
-  ## bisulfite conversion type I
-  cm <- c(
-    cm,
-    bisulfite1=mean(
-      ctls[['BISULFITE CONVERSION I']][sprintf('BS.Conversion.I.C%s', 1:3),'G'] +
-        ctls[['BISULFITE CONVERSION I']][sprintf('BS.Conversion.I.C%s', 4:6),'R'],
-      na.rm=TRUE))
-
-  ## staining
-  cm <- c(cm, stain.red=ctls[['STAINING']]['DNP..High.', 'R'],
-          stain.green=ctls[['STAINING']]['Biotin..High.','G'])
-
-  ## extension
-  cm <- c(cm,
-          extRed1=ctls[['EXTENSION']]['Extension..A.','R'],
-          extRed2=ctls[['EXTENSION']]['Extension..T.','R'],
-          extGrn1=ctls[['EXTENSION']]['Extension..C.','G'],
-          extGrn2=ctls[['EXTENSION']]['Extension..G.','G'])
-
-  ## hybridization
-  d <- ctls[['HYBRIDIZATION']]$G
-  cm <- c(cm, setNames(d, paste0('hybe',1:length(d))))
-
-  ## target removal
-  d <- ctls[['TARGET REMOVAL']]$G
-  cm <- c(cm, setNames(d, paste0('targetrem',1:length(d))))
-
-  ## non-polymorphic
-  cm <- c(cm,
-          nonpolyRed1=ctls[['NON-POLYMORPHIC']]['NP..A.','R'],
-          nonpolyRed2=ctls[['NON-POLYMORPHIC']]['NP..T.','R'],
-          nonpolyGrn1=ctls[['NON-POLYMORPHIC']]['NP..C.','G'],
-          nonpolyGrn2=ctls[['NON-POLYMORPHIC']]['NP..G.','G'])
-
-  ## specificity type II
-  d <- ctls[['SPECIFICITY II']]
-  cm <- c(cm,
-          structure(d$G, names=paste0('spec2Grn', 1:dim(d)[1])),
-          structure(d$R, names=paste0('spec2Red', 1:dim(d)[1])))
-  cm <- c(cm, spec2.ratio = mean(d$G,na.rm=TRUE) / mean(d$R,na.rm=TRUE))
-
-  ## specificity type I green
-  d <- ctls[['SPECIFICITY I']][sprintf('GT.Mismatch.%s..PM.',1:3),]
-  cm <- c(cm, structure(d$G, names=paste0('spec1Grn',1:dim(d)[1])))
-  cm <- c(cm, spec1.ratio1 = mean(d$R, na.rm=TRUE)/mean(d$G, na.rm=TRUE))
-
-  ## specificity type I red
-  d <- ctls[['SPECIFICITY I']][sprintf('GT.Mismatch.%s..PM.',4:6),]
-  cm <- c(cm, structure(d$R, names=paste0('spec1Red',1:dim(d)[1])))
-  cm <- c(cm, spec1.ratio2 = mean(d$G, na.rm=TRUE)/mean(d$R, na.rm=TRUE))
-
-  ## average specificity ratio
-  cm <- c(cm, spec1.ratio = unname((cm['spec1.ratio1']+cm['spec1.ratio2'])/2.0))
-
-  ## normalization
-  cm <- c(cm, c(normA=mean(ctls[['NORM_A']]$R, na.rm=TRUE), 
-                normT=mean(ctls[['NORM_T']]$R, na.rm=TRUE), 
-                normC=mean(ctls[['NORM_C']]$G, na.rm=TRUE), 
-                normG=mean(ctls[['NORM_G']]$G, na.rm=TRUE)))
-
-  cm <- c(cm, dyebias=(cm['normC']+cm['normG']) / (cm['normA']+cm['normT']))
-
-  ## out-of-band probe quantiles
-  if (is.null(sset$oobG)) {
-    cm <- c(cm, oob.ratio=NA, structure(rep(NA,3), names=paste0('oob',c(1,50,99))))
-  } else {
-    cm <- c(cm, oob.ratio=median(sset$oobG) / median(sset$oobR))
-    cm <- c(cm, structure(quantile(sset$oobG, c(0.01,0.5,0.99)), names=paste0('oob', c(1,50,99))))
-  }
-
-  cm
-}
 
 #' build quantiles for funnorm
 #' 
@@ -127,7 +39,7 @@ BuildControlMatrix450k <- function(sset) {
 #' @export
 BuildQuantiles450k <- function(sset, n=500) {
 
-  probe2chr <- GetBuiltInData('hg19.probe2chr', sset$platform)
+  probe2chr <- getBuiltInData('hg19.probe2chr', sset$platform)
 
   probs <- seq(from=0, to=1, length.out=n)
 
@@ -249,7 +161,7 @@ FunnormRegress <- function(cms, qntiles, genders=NULL, k=2) {
 #' @export
 QuantilesInterpolateSignal <- function(sset, qntiles) {
 
-  probe2chr <- GetBuiltInData('hg19.probe2chr', sset$platform)
+  probe2chr <- getBuiltInData('hg19.probe2chr', sset$platform)
   auto.IG <- sset$IG[!(probe2chr[rownames(sset$IG)] %in% c('chrX','chrY')),]
   auto.IR <- sset$IR[!(probe2chr[rownames(sset$IR)] %in% c('chrX','chrY')),]
   auto.II <- sset$II[!(probe2chr[rownames(sset$II)] %in% c('chrX','chrY')),]
@@ -284,7 +196,6 @@ QuantilesInterpolateSignal <- function(sset, qntiles) {
   qntiles.densified <- do.call(c, lapply(1:(n-1), function(i) {
     seq(qntiles[i], qntiles[i+1], (qntiles[i+1]-qntiles[i])/n)[-(n+1)]
   }))
-  library(preprocessCore)
   preprocessCore::normalize.quantiles.use.target(matrix(signal), qntiles.densified)
 }
 
@@ -361,7 +272,7 @@ Funnorm <- function(ssets) {
   ## normalize autosomes and chromosome X
   cms <- lapply(ssets, BuildControlMatrix450k)
   qntiles <- lapply(ssets, BuildQuantiles450k)
-  genders <- InferGenders(ssets)
+  genders <- inferGenders(ssets)
   qntiles.n <- FunnormRegress(cms, qntiles, genders$genders)
   ssets.autoX.n <- Map(QuantilesInterpolateSignal, ssets, qntiles.n)
 
