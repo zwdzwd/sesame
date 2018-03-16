6b53863de16770173f8bb7c10c37dac6a68a8554
Wanding Zhou <wanding.zhou@vai.org>
2018-03-16 11:05:30 -0400

    added Horvath's age predictor

---
+++ R/age.R
@@ -0,0 +1,35 @@
+Hv.age2response <- function(x, adult.age=20) {
+    ## trafo
+    x=(x+1)/(adult.age+1)
+    ifelse(x<=1,log(x),x-1)
+}
+
+Hv.response2age <- function(x, adult.age=20) {
+    ## anti.trafo
+    ifelse(
+        x<0,
+        (1+adult.age)*exp(x)-1,
+        (1+adult.age)*x+adult.age)
+}
+
+#' Horvath 353 age predictor
+#'
+#' @param betas a probeID-named vector of beta values
+#' @return age in years
+#' @export
+#' @examples
+#' betas <- SeSAMeGetExample('HM450.betas.TCGA-2L-AAQA-01A-21D-A38H-05')
+#' predictAgeHorvath353(betas)
+predictAgeHorvath353 <- function(betas) {
+
+    Hv <- getBuiltInData('Horvath353', subdir='age')
+    probes <- intersect(names(na.omit(betas)), Hv$CpGmarker[-1])
+
+    if (length(probes) < 10) {
+        stop('Fewer than 10 matching probes left. Age prediction abort.')
+    }
+
+    drop(Hv.response2age(
+        Hv$CoefficientTraining[1] + 
+            Hv$CoefficientTraining[match(probes, Hv$CpG)] %*% betas[probes]))
+}
