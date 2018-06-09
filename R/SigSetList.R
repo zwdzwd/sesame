ef391c3a6e6ab7d642ca049bd1f18d6c4b23ec94
ttriche <tim.triche@gmail.com>
2018-06-09 18:11:35 -0400

    debuggina and slimming down SigSetList

--- R/SigSetList.R
+++ R/SigSetList.R
@@ -5,37 +5,24 @@
 #' 
 #' @exportClass SigSetList
 setClass("SigSetList",
-         representation(
-          mask="Matrix",
-          platform="character"
-         ),
+         representation(platform="character"),
          contains="SimpleList")
 
 
 #' constructor
 #' 
 #' @param   ...       the SigSet objects that will be the List elements
-#' @param   pCutoff   (optional) if not NULL, mask probes with poobah >= pCutoff
 #' 
 #' @return            a SigSetList 
 #'
 #' @export
-SigSetList <- function(..., pCutoff=NULL) { 
-  platform <- unique(sapply(List(...), "slot", "platform"))
+SigSetList <- function(...) { 
+  platform <- unique(sapply(list(...), slot, "platform"))
   if (length(platform) > 1) {
     stop("All SigSet elements in a SigSetList must be from the same platform")
   }
-  probes <- switch(platform, 
-                   EPIC=865918,
-                   HM450=485577,
-                   HM27=27578)
-  mask <- Matrix(0, nrow=probes, ncol=length(List(...)), sparse=TRUE) 
-  if (!is.null(pCutoff)) {
-    message("Soft-masking probes where pval > ", pCutoff, "...")
-  }
   new("SigSetList", 
       List(...),
-      mask=mask,
       platform=platform,
       elementType="SigSet")
 }
