e89744eb933f618b44a1f76dd83d1aeea4ddfd19
Wubin Ding <ding_wu_bin@163.com>
2021-04-09 18:54:22 +0800

    Update species.r
    Add pvalue

--- R/species.r
+++ R/species.r
@@ -5,7 +5,6 @@
 #' @param df_as a data.frame of alignment score for each probe.
 #' @param hm.pos.por whether to use the postive probes portion to infer human.
 #' @param topN Top n positive and negative probes used to infer species.
-#' @param plot Whether to plot ROC curve.
 #' @return species (NCBI official species names)
 #' We infer species based on probes pvalues and alignment score.
 #' We calculate AUC for each species, y_true is whether pval < 0.01
@@ -29,9 +28,14 @@
 #' samplesheet="/mnt/isilon/zhoulab/labsamplesheets/2021/20210325_Mammal40_Merged_GEO_Steve_Wubin_samplesheets.xlsx"
 #' 
 #' sample_info=read_excel(samplesheet)
-#' df_as=t(read.table(AS_dir,header=T,row.names=1,sep='\t'))
-#' overlapped_species=intersect(unique(sample_info$NCBI_scientific_name),colnames(df_as))
-#' sample_info=sample_info[sample_info$NCBI_scientific_name %in% overlapped_species,]
+#' df_as=read.table(AS_dir,header=T,sep='\t')
+#' rownames(df_as) <- do.call(paste,c(df_as[c("Species","taxid")],sep="|"))
+#' df_as <- t(df_as[,!names(df_as) %in% c("Species","taxid")])
+#' df_as[1:3,1:3]
+#' 
+#' sample_info$IDs <- do.call(paste,c(sample_info[c("NCBI_scientific_name","taxid")],sep="|"))
+#' overlapped_species=intersect(unique(IDs),colnames(df_as))
+#' sample_info=sample_info[sample_info$IDs %in% overlapped_species,]
 #' df_as=df_as[,overlapped_species]
 #' df_as=df_as / 50
 #' idats=searchIDATprefixes(indir)
@@ -41,33 +45,28 @@
 #' mft <- readRDS('/mnt/isilon/zhoulab/labprojects/20201124_Horvath_mammal_array/mammal_array_manifest_original.rds')
 #' sset=readIDATpair(idats[1],manifest=mft, platform='mammal')
 #' 
-#' result=do.call(cbind, mclapply(idats, function(x) {
-#'     sset=readIDATpair(x,manifest=mft, platform='mammal')
-#'     AUC=inferSpecies(sset,df_as,hm.pos.por=F,topN=2000,plot=F,ret.max=F)
-#' },mc.cores=8)
-#' )
-#' write.table(as.data.frame(result),file="AUC_for_species.txt",sep='\t',quote=FALSE,col.names=T)
-
 #' @export
-inferSpecies <- function(sset,df_as=NULL,hm.pos.por=F,topN=2000,plot=F,ret.max=T) {
+inferSpecies <- function(sset,df_as=NULL,hm.pos.por=T,topN=3000,ret.max=T) {
     pvalue=pval(sset)
+    pvalue <- pvalue[intersect(names(pvalue),rownames(df_as))]
     N=length(pvalue)
     pos_probes=sort(pvalue[pvalue< 0.01],decreasing=F)
-    n_pos=length(pos_probes)
-    if( hm.pos.por & (n_pos / N > 0.8)){
-        return("Homo sapiens")
-        }
     neg_probes=sort(pvalue[pvalue > 0.2],decreasing=T)
-    if(n_pos > topN){
+    n_pos=length(pos_probes)
+    n_neg=length(neg_probes)
+    if (hm.pos.por & (n_neg / N < 0.1)){
+        return('Homo sapiens|9606')
+        }
+    if (n_pos > topN){
         pos_probes=pos_probes[1:topN]
         }
-    if(length(neg_probes) > topN){
+    if (n_neg > topN){
         neg_probes=neg_probes[1:topN]
         }
-    probes=intersect(row.names(df_as),c(names(pos_probes),names(neg_probes)))
+    probes=c(names(pos_probes),names(neg_probes))
     y_true=sapply(pvalue[probes],function(x) {if (x<0.01) {return(1)} else {return(0)}})
     #y_true=c(rep(1,length(pos_probes)),rep(0,length(neg_probes)))
-    if(length(y_true) == 0){
+    if (length(y_true) == 0){
         if (ret.max==T){
             return(NA)
             }
@@ -76,22 +75,26 @@ inferSpecies <- function(sset,df_as=NULL,hm.pos.por=F,topN=2000,plot=F,ret.max=T
             }
         }
         
-    AUC=sapply(as.data.frame(df_as[probes,]),function(x) {
+    
+    res = as.data.frame(t(sapply(colnames(df_as),function(species) {
+            x=df_as[probes,species]
             if(length(x) == 0){
-                return(NA)
+                return(list(auc=NA,p.value=1))
                 }
-            PRROC_obj <- roc.curve(scores.class0 = x, weights.class0=y_true,curve=plot)
-            if (plot){
-                plot(PRROC_obj)
-                }
-            return(PRROC_obj$auc)
-        })
+            PRROC_obj <- roc.curve(scores.class0 = x, weights.class0=y_true,curve=F)
+            p <- wilcox.test(x[names(pos_probes)],x[names(neg_probes)],alternative='greater')
+            return(list(auc=PRROC_obj$auc,p.value=p$p.value))
+        })))
+        
+
     if (ret.max == T){
-        return(names(AUC)[which.max(AUC)])
-        #What if there are two maximal AUC?
+        r=t(res[which.max(res$auc),])
+        l=r[,1]
+        l['species']=unlist(strsplit(colnames(r)[1],"\\|"))[1]
+        l['taxid']=unlist(strsplit(colnames(r)[1],"\\|"))[2]
+        return(l)
         }
     else {
-        return(AUC)
+        return(res)
         }
 }
-
