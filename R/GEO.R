9a67bdbef0beb5a102e81de6fd382e941f9de859
Wanding Zhou <zhouwanding@gmail.com>
2022-02-14 17:23:59 -0500

    minor, message omitted

--- R/GEO.R
+++ R/GEO.R
@@ -2,12 +2,14 @@
 #' Convert signal M and U to SigDF
 #'
 #' This overcomes the issue of missing IDAT files. However,
-#' out-of-band signals will be missing or faked.
+#' out-of-band signals will be missing or faked (sampled from a
+#' normal distribution).
 #' 
 #' @param sigM methylated signal, a numeric vector
 #' @param sigU unmethylated signal, a numirc vector
 #' @param Probe_IDs probe ID vector
-#' @param oob assumed value for out-of-band signals
+#' @param oob.mean assumed mean for out-of-band signals
+#' @param oob.sd assumed standard deviation for out-of-band signals
 #' @return SigDF
 #' @examples
 #' sigM <- c(11436, 6068, 2864)
@@ -15,16 +17,19 @@
 #' probes <- c("cg07881041", "cg23229610", "cg03513874")
 #' sdf <- parseGEOsignalMU(sigM, sigU, probes)
 #' @export
-parseGEOsignalMU <- function(sigM, sigU, Probe_IDs, oob=500) {
+parseGEOsignalMU <- function(
+    sigM, sigU, Probe_IDs, oob.mean = 500, oob.sd = 300) {
+    
     platform <- inferPlatformFromProbeIDs(Probe_IDs)
     addr <- sesameDataGet(paste0(platform, ".address"))$ordering
     M <- sigM[match(addr$Probe_ID, Probe_IDs)]
     U <- sigU[match(addr$Probe_ID, Probe_IDs)]
     col <- ifelse(is.na(addr$col), "2", as.character(addr$col))
-    MG <- ifelse(col == "2", NA, ifelse(col == "G", M, oob))
-    MR <- ifelse(col == "2", NA, ifelse(col == "R", M, oob))
-    UG <- ifelse(col == "2", M, ifelse(col == "G", U, oob))
-    UR <- ifelse(col == "2", U, ifelse(col == "R", U, oob))
+    oobs <- pmax(50,rnorm(length(col), mean = oob.mean, sd = oob.sd))
+    MG <- ifelse(col == "2", NA, ifelse(col == "G", M, oobs))
+    MR <- ifelse(col == "2", NA, ifelse(col == "R", M, oobs))
+    UG <- ifelse(col == "2", M, ifelse(col == "G", U, oobs))
+    UR <- ifelse(col == "2", U, ifelse(col == "R", U, oobs))
     sdf <- data.frame(Probe_ID = addr$Probe_ID,
         MG = MG, MR = MR, UG = UG, UR = UR,
         col = factor(col, levels=c("G","R","2")), mask = addr$mask)
