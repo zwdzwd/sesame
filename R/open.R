45d199f6e817f07db076fce88ef7d199e9897268
Wanding Zhou <zhouwanding@gmail.com>
2022-02-20 09:41:52 -0500

    added prepSesameList

--- R/open.R
+++ R/open.R
@@ -1,5 +1,33 @@
+#' List supported prepSesame functions
+#'
+#' @return a data frame with code, func, description
+#' @examples
+#' prepSesameList()
+#' @export
+prepSesameList <- function() {
+    x <- data.frame(rbind(
+        c("Q", "qualityMask", "Mask probes of poor design"),
+        c("U", "prefixMaskAllButCs", "Mask all but cg- and ch-probes"),
+        c("C", "inferInfiniumIChannel", "Infer channel for Infinium-I probes"),
+        c("D", "dyeBiasNL", "Dye bias correction (non-linear)"),
+        c("P", "pOOBAH", "Detection p-value masking using oob"),
+        c("B", "noob", "Background subtraction using oob"),
+        c("S", "inferSpecies", "Set species-specific mask"),
+        c("T", "inferStrain", "Set strain-specific mask"),
+        c("M", "matchDesign", "Match Inf-I/II in beta distribution")))
+    colnames(x) <- c("code", "func", "description")
+    x
+}
 
 #' Apply a chain of sesame preprocessing functions in an arbitrary order
+#' Notes on the order of operation:
+#'
+#' 1. qualityMask and inferSpecies should go before noob and pOOBAH,
+#' otherwise the background is too high because of Multi,
+#' uk and other probes
+#' 2. dyeBias correction needs to happen early
+#' 3. channel inference before dyebias
+#' 4. noob should happen last, pOOBAH before noob because noob modifies oob
 #' 
 #' @param sdf SigDF
 #' @param code code that indicates preprocessing functions and their
@@ -10,22 +38,13 @@
 #' sdf1 <- prepSesame(sdf, "QCDPB")
 #' @export
 prepSesame <- function(sdf, code = "QCDPB") {
-    cfuns <- list(
-        Q = qualityMask,
-        U = prefixMaskCtlUK,
-        C = inferInfiniumIChannel,
-        D = dyeBiasNL,
-        P = pOOBAH,
-        B = noob,
-        S = inferSpecies,
-        T = inferStrain,
-        M = matchDesign)
+    cfuns <- prepSesameList()
     
     codes <- str_split(code,"")[[1]]
-    stopifnot(all(codes %in% names(cfuns)))
+    stopifnot(all(codes %in% cfuns$code))
     x <- sdf
-    for(code1 in codes) {
-        x <- cfuns[[code1]](x) }
+    for(c1 in codes) {
+        x <- get(cfuns[cfuns$code == c1, "func"])(x) }
     x
 }
 
@@ -36,18 +55,10 @@ prepSesame <- function(sdf, code = "QCDPB") {
 #' 
 #' If the input is an IDAT prefix or a \code{SigDF}, the output is
 #' the beta value numerics.
-#'
-#' Notes on the order of operation:
-#' 1. qualityMask and inferSpecies should go before noob and pOOBAH,
-#' otherwise the background is too high because of Multi,
-#' uk and other probes
-#' 2. dyeBias correction needs to happen early
-#' 3. channel inference before dyebias
-#' 4. noob should happen last, pOOBAH before noob because noob modifies oob
 #' 
 #' @param x SigDF(s), IDAT prefix(es)
 #' @param platform optional platform string
-#' @param preprocess preprocessing spell, see preprocessSesame()
+#' @param prep preprocessing code, see ?prepSesame
 #' @param manifest optional dynamic manifest
 #' @param func either getBetas or getAFs
 #' @param ... parameters to getBetas
@@ -61,7 +72,7 @@ prepSesame <- function(sdf, code = "QCDPB") {
 #' betas <- openSesame(IDATprefixes)
 #' @export
 openSesame <- function(
-    x, platform = '', preprocess = "QCDPB", manifest = NULL,
+    x, platform = '', prep = "QCDPB", manifest = NULL,
     func = getBetas, BPPARAM=SerialParam(), ...) {
 
     ## expand if a directory
@@ -70,11 +81,11 @@ openSesame <- function(
     }
 
     if (is(x, "SigDF")) {
-        func(prepSesame(x, preprocess), ...)
+        func(prepSesame(x, prep), ...)
     } else if (is(x, 'character')) {
         if (length(x) == 1) {
             func(prepSesame(readIDATpair(
-                x, platform = platform, manifest = manifest), preprocess), ...)
+                x, platform = platform, manifest = manifest), prep), ...)
         } else { # multiple IDAT prefixes / SigDFs
             do.call(
                 cbind, bplapply(x, openSesame,
