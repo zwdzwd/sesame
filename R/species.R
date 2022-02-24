98dafe53eaa54f4d220d15ee8efcf6949d15e194
Wanding Zhou <zhouwanding@gmail.com>
2022-02-23 22:54:19 -0500

    minor, message omitted

--- R/species.R
+++ R/species.R
@@ -137,8 +137,11 @@ inferSpecies <- function(sdf, topN = 1000,
         U1/(n1 * n2)}, numeric(1))
 
     ## if success rate is high but max(AUC) is low, use reference.
-    if ((success.rate >= ref_detection_rate && max(auc) < 0.8) || (
-        success.rate >= ref_detection_rate * 0.9 && max(auc) < 0.8*0.9)) {
+    ## if ((success.rate >= ref_detection_rate && max(auc) < 0.8) || (
+    ##     success.rate >= ref_detection_rate * 0.9 && max(auc) < 0.8*0.9)) {
+    if (success.rate >= 0.95 || 
+            (success.rate >= 0.9 && max(auc) < 0.8) ||
+            (success.rate >= 0.85 && max(auc) < 0.7)) {            
         ## success.rate >= ref_detection_rate * 0.9 &&
         ##     auc[addr$reference] > quantile(auc, 0.75) &&
         ##     max(auc) < ref_max_auc)) {
