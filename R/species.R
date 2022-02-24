611e4844929d0f2dc85e16b3aacf134f8011cfda
Wanding Zhou <zhouwanding@gmail.com>
2022-02-23 21:57:20 -0500

    minor, message omitted

--- R/species.R
+++ R/species.R
@@ -138,9 +138,10 @@ inferSpecies <- function(sdf, topN = 1000,
 
     ## if success rate is high but max(AUC) is low, use reference.
     if ((success.rate >= ref_detection_rate && max(auc) < 0.8) || (
-        success.rate >= ref_detection_rate * 0.9 &&
-            auc[addr$reference] > quantile(auc, 0.75) &&
-            max(auc) < ref_max_auc)) {
+        success.rate >= ref_detection_rate * 0.9 && max(auc) < 0.8*0.9)) {
+        ## success.rate >= ref_detection_rate * 0.9 &&
+        ##     auc[addr$reference] > quantile(auc, 0.75) &&
+        ##     max(auc) < ref_max_auc)) {
         message("Lack of negative probes. Use reference.")
         species <- addr$reference
         if (return.auc){ return(auc);
