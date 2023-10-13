134f6f9ace7f02f98cf49c9fe1a6c76cabec79f1
Wanding Zhou <zhouwanding@gmail.com>
2023-10-13 16:17:23 -0400

    minor, message omitted

--- R/impute.R
+++ R/impute.R
@@ -1,3 +1,37 @@
+#' Convert human arrays to previous platforms
+#' Missing probes are replaced using NAs.
+#'
+#' @param sdf SigDF data frame
+#' @param platform HM450 or EPIC
+#' @return a new SigDF for the older platform
+#' @examples
+#' sdf <- sesameDataGet("EPIC.5.SigDF.normal")[[1]]
+#' sdf_out <- convertTo(sdf, "HM450")
+#' @export
+convertTo <- function(sdf, platform=c("HM450", "EPIC")) {
+    probes <- sdf$Probe_ID
+    if (sdfPlatform(sdf) == "EPICv2") {
+        probes <- vapply(strsplit(probes, "_"),
+            function(x) x[1], character(1))
+    }
+
+    if (platform == "HM450") {
+        sdf_ref <- sesameDataGet("HM450.10.SigDF")[[1]]
+    } else if (platform == "EPIC") {
+        sdf_ref <- sesameDataGet("EPIC.5.SigDF.normal")[[1]]
+    }
+
+    idx <- match(sdf_ref$Probe_ID, probes)
+    sdf_out <- sdf_ref
+    sdf_out$MG <- sdf$MG[idx]
+    sdf_out$MR <- sdf$MR[idx]
+    sdf_out$UG <- sdf$UG[idx]
+    sdf_out$UR <- sdf$UR[idx]
+    sdf_out$col <- sdf$col[idx]
+    sdf_out$mask <- sdf$mask[idx]
+    sdf_out$mask[is.na(sdf_out$mask)] <- TRUE
+    sdf_out
+}
 
 #' Impute to platform
 #' 
